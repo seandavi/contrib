478ff317a073badbff16b277cde4bb5f54c6c5f8
Sean Davis <seandavi@gmail.com>
2015-08-21 01:04:49 -0400

    Update genresp.sh

--- irods-docker/icat/genresp.sh
+++ irods-docker/icat/genresp.sh
@@ -16,6 +16,8 @@ echo "/var/lib/irods/Vault" >> $RESPFILE # vault path
 (openssl rand -base64 16 | sed 's,/,S,g' | cut -c 1-16 \
  | tr -d '\n' ; echo "-SID") >> $RESPFILE # zone SID
 openssl rand -base64 32 | sed 's,/,S,g' | cut -c 1-32 >> $RESPFILE # agent key
+echo "1248" >> $RESPFILE                  # control port
+openssl rand -base64 32 | sed 's,/,S,g' | cut -c 1-32 >> $RESPFILE # control plane key
 echo "rods" >> $RESPFILE                  # iRODS admin account
 openssl rand -base64 16 | sed 's,/,S,g' | cut -c 1-16 >> $RESPFILE      # iRODS admin password
 echo "yes" >> $RESPFILE                   # confirm iRODS settings
